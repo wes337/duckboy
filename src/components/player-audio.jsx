bce68f478541146036c3ce2ccda84c845f3ff6f6
wes337 <wesmoses@gmail.com>
2025-03-30 14:04:41 +0300

    audio player inner adjust

--- src/components/player-audio.jsx
+++ src/components/player-audio.jsx
@@ -50,6 +50,37 @@ export default function PlayerContent() {
 
   return (
     <div classList={{ [styles.playerAudio]: true, [styles.show]: showAudio() }}>
+      <div class={styles.bumperBackground}>
+        <video src={bumper()} autoplay muted loop playsinline />
+      </div>
+      <div class={styles.bumper}>
+        <video src={bumper()} autoplay muted loop playsinline />
+      </div>
+      <div class={styles.artist}>DUCKBOY</div>
+      <div class={styles.name}>
+        <marquee>{currentTrack().name}</marquee>
+      </div>
+      <div class={styles.seek}>
+        <img class={styles.bar} src={`/player/seek-bar.png`} />
+        <img
+          class={styles.knob}
+          src={`/player/seek-knob.png`}
+          style={{ left: `${AudioPlayer.seek}%` }}
+        />
+      </div>
+      <div class={styles.duration}>
+        <div>{AudioPlayer.currentTime}</div>/<div>{AudioPlayer.duration}</div>
+      </div>
+
+      <img class={styles.borderTop} src={`/patterns/gold-border-y.png`} />
+      <img class={styles.borderBottom} src={`/patterns/gold-border-y.png`} />
+      <img class={styles.borderLeft} src={`/patterns/gold-border-x.png`} />
+      <img class={styles.borderRight} src={`/patterns/gold-border-x.png`} />
+    </div>
+  );
+
+  return (
+    <div classList={{ [styles.playerAudio]: true, [styles.show]: showAudio() }}>
       <div class={styles.inner}>
         {bumper() && (
           <div class={styles.bumperBackground}>
